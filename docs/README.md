9bfe3a5ee365234ee39ba64f756860999917d37a
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-04-29 17:02:50 -0400

    experiment with draw io embeddings

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+![drawio diagram](system_design.drawio)
---
+++ validation_pytorch.py
@@ -0,0 +1,14 @@
+class Foo:
+    def __init__(self) -> None:
+        pass
+
+    def start(self):
+        pass
+
+
+class Bar:
+    def __init__(self) -> None:
+        pass
+
+    def end(self):
+        pass
